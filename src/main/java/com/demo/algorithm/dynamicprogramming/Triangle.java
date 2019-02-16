6cd340367d80310de881833891adf2ed9c027f11
peichunle <peichunle@163.com>
2019-02-16 19:19:56 +0800

    Update Triangle.java

--- src/main/java/com/demo/algorithm/dynamicprogramming/Triangle.java
+++ src/main/java/com/demo/algorithm/dynamicprogramming/Triangle.java
@@ -1,5 +1,7 @@
 package com.demo.algorithm.dynamicprogramming;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -8,12 +10,40 @@ import java.util.List;
 public class Triangle {
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        for (int i = 1; i < triangle.size(); i++) {
-            triangle.get(i);
-            for (int j = 0; j < triangle.get(i).size(); j++) {
-
+        int n = triangle.size();
+        if(n==0){
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        int[] road = new int[n];
+        road[0] = triangle.get(0).get(0);
+        if(n==1){
+            return road[0];
+        }
+        road[1] = road[0]+triangle.get(1).get(1);
+        road[0] = road[0]+triangle.get(1).get(0);
+        if(n==2){
+            return Math.min(road[0],road[1]);
+        }
+        for(int i = 2;i < n;i++){
+            road[i] = road[i-1] + triangle.get(i).get(i);
+            for(int j = i-1;j >= 1;j--){
+                road[j] = Math.min(road[j],road[j-1])+triangle.get(i).get(j);
+            }
+            road[0] = road[0]+triangle.get(i).get(0);
+        }
+        for(int i = 0;i < n;i++){
+            if(road[i] < min){
+                min = road[i];
             }
         }
-        return -1;
+        return min;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        int i = new Triangle().minimumTotal(triangle);
+        System.out.println(i);
     }
 }
